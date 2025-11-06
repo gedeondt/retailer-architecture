83a3594d08777b37c788b43a64e684164a69d0b0
Gedeón Domínguez Torán <gedeon.dominguez@clouddistrict.com>
2025-11-06 07:07:11 +0100

    Fijar origen API por defecto del widget ecommerce

--- dashboard/scripts/dominios-page.js
+++ dashboard/scripts/dominios-page.js
@@ -2,9 +2,11 @@ import dashboardWidgets from './widget-loader.js';
 
 function initDominiosWidgets() {
   const baseOrigin = window.location.origin.replace(/\/$/, '');
+  const defaultApiOrigin = 'http://127.0.0.1:4300';
   dashboardWidgets.mountWidget({
     slotId: 'ventasdigitales-ecommerce-widget-slot',
     defaultWidgetOrigin: `${baseOrigin}/widgets/ventasdigitales/ecommerce/`,
+    defaultApiOrigin,
     errorTitle: 'Widget Ecommerce no disponible',
   });
 }
